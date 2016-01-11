df8f2cee5af7469340db508282f74bdb6e8b5b2b
Christoph Wagner <christoph@Christophs-MacBook.local>
2016-01-11 21:34:15 +0100

    Grundgerüst

---
+++ inc/matrix.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+// festlegen den N-dimensionalen Raumes
+const unsigned int N = 3;
+
+// definieren des Datentyps VectorN
+typedef double VectorN[N];
+
+#endif /* MATRIX_H */
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+// main.cpp
+
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
